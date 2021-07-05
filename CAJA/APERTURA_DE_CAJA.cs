43befd185e36254adbcf0f1b1118e84d75ece67a
morgadohj <47167868+morgadohj@users.noreply.github.com>
2021-07-05 11:19:35 -0500

    MODULO CAJA DISEÑO

---
+++ CAJA/APERTURA_DE_CAJA.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MDsystems.CAJA
+{
+    public partial class APERTURA_DE_CAJA : Form
+    {
+        public APERTURA_DE_CAJA()
+        {
+            InitializeComponent();
+        }
+
+        private void APERTURA_DE_CAJA_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
