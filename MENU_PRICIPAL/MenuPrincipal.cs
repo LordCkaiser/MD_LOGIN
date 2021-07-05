afb0e4d8dd35002f34e058d8f2fc5b80a927033a
morgadohj <47167868+morgadohj@users.noreply.github.com>
2021-07-05 11:29:31 -0500

    AGREGUE LOS FORM

---
+++ MENU_PRICIPAL/MenuPrincipal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace MD_LOGIN.MENU_PRICIPAL
+{
+    public partial class MenuPrincipal : Form
+    {
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void MenuPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
