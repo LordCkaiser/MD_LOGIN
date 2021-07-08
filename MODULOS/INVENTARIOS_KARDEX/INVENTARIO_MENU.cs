433ce1a489f57407b0831e665c4df9efac56f651
morgadohj <morgadohj@hotmail.com>
2021-07-08 15:30:36 -0500

    diseño de boton de movimientos

---
+++ MODULOS/INVENTARIOS_KARDEX/INVENTARIO_MENU.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MD_LOGIN.MODULOS.INVENTARIOS_KARDEX
+{
+    public partial class INVENTARIO_MENU : Form
+    {
+        public INVENTARIO_MENU()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel10_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
